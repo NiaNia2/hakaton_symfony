5b75665c1e42353a21dc4c4f3cae90313c2f623d
Kevin Zugasti <bearded-developer@protonmail.com>
2025-08-22 17:48:31 +0200

    add: migration with units

--- src/Controller/GamesController.php
+++ src/Controller/GamesController.php
@@ -8,7 +8,15 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class GamesController extends AbstractController
 {
-    #[Route('/games', name: 'app_games')]
+    #[Route('/hub/{id}', name: 'hub')]
+    public function hub(): Response
+    {
+        return $this->render('games/index.html.twig', [
+            'controller_name' => 'GamesController',
+        ]);
+    }
+
+    #[Route('/games', name: 'games')]
     public function index(): Response
     {
         return $this->render('games/index.html.twig', [
