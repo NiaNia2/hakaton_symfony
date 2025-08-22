469e6750f6e045be39122ee9a543516d7903de47
Kevin Zugasti <bearded-developer@protonmail.com>
2025-08-22 19:33:54 +0200

    add: padding and fix nav

--- src/Controller/GamesController.php
+++ src/Controller/GamesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,17 +10,19 @@ use Symfony\Component\Routing\Attribute\Route;
 final class GamesController extends AbstractController
 {
     #[Route('/hub/{id}', name: 'hub')]
-    public function hub(): Response
+    public function hub(User $user): Response
     {
-        return $this->render('games/index.html.twig', [
-            'controller_name' => 'GamesController',
+        $team = $user->getTeams();
+        return $this->render('games/hub.html.twig', [
+            'team' => $team,
+            'user' => $user,
         ]);
     }
 
     #[Route('/games', name: 'games')]
     public function index(): Response
     {
-        return $this->render('games/index.html.twig', [
+        return $this->render('games/game.html.twig', [
             'controller_name' => 'GamesController',
         ]);
     }
