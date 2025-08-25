054e20aabfa939f8d4a2155ddd0be441b574cb72
Kevin Zugasti <bearded-developer@protonmail.com>
2025-08-25 22:23:59 +0200

    units and pre animations

--- src/Controller/GamesController.php
+++ src/Controller/GamesController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Repository\TeamsRepository;
 use App\Repository\UnitsRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,40 +19,95 @@ final class GamesController extends AbstractController
         $type = $request->get('type');
 
         if (!$type) {
-            return new Response('', 200);
+            return new Response('', Response::HTTP_NO_CONTENT);
         }
 
-        $types = $unitsRepository->createQueryBuilder('u')
-            ->select('DISTINCT u.type')
-            ->getQuery()
-            ->getResult();
+        $colors = $unitsRepository->findBy(['type' => $type], ['color' => 'ASC']);
+
         $team = $teamsRepository->findOneBy(['user' => $this->getUser()]);
+        $unitOneType   = $team?->getUnitOne()?->getType();
+        $unitOneColor  = $team?->getUnitOne()?->getColor();
+        $unitTwoType   = $team?->getUnitTwo()?->getType();
+        $unitTwoColor  = $team?->getUnitTwo()?->getColor();
+        $unitThreeType = $team?->getUnitThree()?->getType();
+        $unitThreeColor= $team?->getUnitThree()?->getColor();
 
         return $this->render('games/units/_color_options.html.twig', [
-            'types' => $types,
-            'unitOneType' => $team?->getUnitOne()?->getType(),
-            'unitOneColor' => $team?->getUnitOne()?->getColor(),
-            'unitTwoType' => $team?->getUnitTwo()?->getType(),
-            'unitTwoColor' => $team?->getUnitTwo()?->getColor(),
-            'unitThreeType' => $team?->getUnitThree()?->getType(),
-            'unitThreeColor' => $team?->getUnitThree()?->getColor(),
+            'colors'         => $colors,
+            'unitOneType'    => $unitOneType,
+            'unitOneColor'   => $unitOneColor,
+            'unitTwoType'    => $unitTwoType,
+            'unitTwoColor'   => $unitTwoColor,
+            'unitThreeType'  => $unitThreeType,
+            'unitThreeColor' => $unitThreeColor,
         ]);
     }
 
     #[Route('/island/{id}', name: 'island')]
     public function island(User $user, Request $request, UnitsRepository $unitsRepository): Response
     {
-        $type = $request->query->get('type');
-        $types = $unitsRepository->createQueryBuilder('u')->select('DISTINCT u.type')->getQuery()->getScalarResult();
+
+        $typesRows = $unitsRepository->createQueryBuilder('unit')
+            ->select('DISTINCT unit.type')
+            ->getQuery()
+            ->getScalarResult();
+        $types = array_map(fn($r) => $r['type'], $typesRows);
         $team = $user->getTeams();
-        $units = $unitsRepository->findBy(["type" => $type]);
+
         return $this->render('games/island.html.twig', [
             'team' => $team,
             'user' => $user,
-            'units' => $units,
             'types' => $types,
-            'type' => $type,
+            'unitOneType'     => $team?->getUnitOne()?->getType(),
+            'unitOneColor'    => $team?->getUnitOne()?->getColor(),
+            'unitTwoType'     => $team?->getUnitTwo()?->getType(),
+            'unitTwoColor'    => $team?->getUnitTwo()?->getColor(),
+            'unitThreeType'   => $team?->getUnitThree()?->getType(),
+            'unitThreeColor'  => $team?->getUnitThree()?->getColor(),
         ]);
+    }
+
+    #[Route('/team/save-unit', name: 'team_save_unit', methods: ['POST'])]
+    public function saveUnit(Request $request, UnitsRepository $unitsRepo, TeamsRepository $teamsRepo, EntityManagerInterface $entityManager,): Response
+    {
+        $slot  = $request->request->get('slot');
+        $type  = $request->request->get('type');
+        $color = $request->request->get('color');
+
+        if (!$slot || !$type || !$color) {
+            return new Response('', Response::HTTP_NO_CONTENT);
+        }
+
+        $team = $teamsRepo->findOneBy(['user' => $this->getUser()]);
+
+        $unit = $unitsRepo->findOneBy(['type' => $type, 'color' => $color]);
+        if (!$unit) {
+            return new Response('Invalid unit', Response::HTTP_BAD_REQUEST);
+        }
+
+        match ($slot) {
+            'one'   => $team->setUnitOne($unit),
+            'two'   => $team->setUnitTwo($unit),
+            'three' => $team->setUnitThree($unit),
+            default => null
+        };
+
+        $entityManager->persist($team);
+        $entityManager->flush();
+
+        $previewId = match ($slot) {
+            'one' => 'unitOnePreview',
+            'two' => 'unitTwoPreview',
+            'three' => 'unitThreePreview',
+            default => null,
+        };
+
+        $html = $this->renderView('games/units/_unit_card_wrapper.html.twig', [
+            'previewId' => $previewId,
+            'unit'      => $unit,
+        ]);
+
+        return new Response($html, Response::HTTP_OK);
     }
 
     #[Route('/games', name: 'games')]
